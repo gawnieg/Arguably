bfbd30f1e3a20f23d8a3dd19eb163cdd39384dca
root <root@csg-xenial-template.doc.ic.ac.uk>
2017-03-10 14:30:11 +0000

    Put in basic nested comment system

--- server.js
+++ server.js
@@ -41,10 +41,18 @@ app.get('/topicspage/:name', function(req, res){
 //	.run("MATCH (n:Opinion) WHERE n.topic = \"" + req.params.name.replace(/_+/g, " ") + "\" RETURN n")
 	.then(function(result){
             //var topicArray =[];
-	    var topicArray =[];
+	    var topicArray =[],
+		testArr = [],
+		printedArr = [];
 
+	    recursivePrint(result.records, 0, printedArr, testArr);
+
+	    testArr.forEach(function (result) {
+		console.log(result.argumenttext);
+	    });
+	    
 	    result.records.forEach(function(record){
-		console.log(record._fields[1]);
+		//console.log(record._fields[1]);
 		topicArray.push({
                     id: record._fields[0].identity.low,
                     argumenttext: record._fields[0].properties.argumenttext,
@@ -66,9 +74,64 @@ app.get('/topicspage/:name', function(req, res){
 
 });
 
+function recursivePrint(records, index, printedArr, topicArr) {
+    console.log(records.length);
+    console.log("printed array length: " + printedArr.length);
+    console.log("index: " + index);
+    //return if index is out of bounds
+    if (index >= records.length) {
+	return;
+    }
+    if (printedArr.indexOf(records[index]._fields[0].identity.low) > -1) {
+	recursivePrint(records,index+1,printedArr,topicArr);
+	//return;
+    }
+    if (printedArr.length >= records.length) {
+	return;
+    }
 
-function isParent(nodeId) {
+    
+    //return if it's already been "printed"
+    
 
+    printedArr.push(records[index]._fields[0].identity.low);
+    topicArr.push({
+        id: records[index]._fields[0].identity.low,
+        argumenttext: records[index]._fields[0].properties.argumenttext,
+        topic: records[index]._fields[0].properties.topic
+    });
+
+    //if node has replies
+    if (records[index]._fields[1].length > 0) {
+	var i;
+	for (i = 0; i < records[index]._fields[1].length; i++) {
+	    //find index of each reply
+	    var replyIndex = findIndex(records, records[index]._fields[1][i].identity.low);
+	    console.log("ReplyIndex: " + replyIndex);
+	    if (replyIndex >= 0) {
+		//print reply
+		recursivePrint(records, replyIndex, printedArr, topicArr);
+	    }
+	}
+    }
+    //else go to next place in array
+    if (records[index]._fields[0].properties.isReply == "True"){
+	return;
+    }
+    
+    else recursivePrint(records,index+1,printedArr,topicArr);
+    
+
+}
+
+function findIndex(records, item) {
+    var i;
+    for (i = 0; i < records.length; i++) {
+	if (records[i]._fields[0].identity.low == item) {
+	    return i;
+	}
+    }
+    return -1;
 }
 
 
@@ -250,7 +313,7 @@ app.post('/opinion/add',function(req,res){
     var topic = req.body.topic;
 
     session
-	.run('CREATE(n:Opinion {argumenttext:{argumenttextParam},topic:{topicParam}}) RETURN n.argumenttext', {argumenttextParam:argumenttext,topicParam:topic})
+	.run('CREATE(n:Opinion {argumenttext:{argumenttextParam},topic:{topicParam},isReply:"False"}) RETURN n.argumenttext', {argumenttextParam:argumenttext,topicParam:topic})
 	.then(function(result){
 	    res.redirect('/');
 	    session.close();
@@ -272,7 +335,7 @@ app.post('/opinion/addArgToTopic',function(req,res){
     var topic = req.body.Topic.replace(/_+/g, " ");
 
     session
-	.run('CREATE(n:Opinion {argumenttext:{argumenttextParam},topic:{topicParam}}) RETURN n.argumenttext', {argumenttextParam:argumenttext,topicParam:topic})
+	.run('CREATE(n:Opinion {argumenttext:{argumenttextParam},topic:{topicParam},isReply:"False"}) RETURN n.argumenttext', {argumenttextParam:argumenttext,topicParam:topic})
 	.then(function(result){
 	    res.redirect('/topicspage/' + topic.replace(/ +/g, "_"));
 	    session.close();
@@ -295,7 +358,7 @@ app.post('/opinion/addReply', function(req,res) {
     session
         .run("MATCH (initNode: Opinion {argumenttext:{initialArgParam}, topic:{topicParam}}) CREATE \
               (initNode) <- [r:REPLY] - (newNode: Opinion {argumenttext:{replyTextParam}, topic: \
-{topicParam}}) CREATE (initNode) <- [rel: " + relType + " ] - (newNode)", {initialArgParam: initialArg, replyTextParam: replyText, topicParam: topic}) 
+{topicParam},isReply:\"True\"}) CREATE (initNode) <- [rel: " + relType + " ] - (newNode)", {initialArgParam: initialArg, replyTextParam: replyText, topicParam: topic}) 
 	.then(function(result){
 	    res.redirect('/topicspage/' + topic.replace(/ +/g, "_"));
 	    session.close();
