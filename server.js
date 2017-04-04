06f87653070469d211bcf63b821fcd4253c2a2ae
root <root@csg-xenial-template.doc.ic.ac.uk>
2017-04-04 18:11:57 +0100

    json now lists arguments with their replies, and the relationship type

--- server.js
+++ server.js
@@ -258,24 +258,62 @@ app.get('/contact_us', function(req, res) {
 
 //SECTION FOR DOWNLOAD ALL BUTTON ON HOMEPAGE
 app.get('/download_all',function(req,res){
-var Download_all_Array=[];
-
+    //declare array for all data and a separate one for replies
+    var DownloadAllArray=[];
+    var replyArray = [];
     session
-        .run('MATCH (n:Opinion) RETURN DISTINCT n.topic LIMIT 100')//can change this query to suit
+        .run('MATCH (n:Opinion) OPTIONAL MATCH (n) <-[r:ATTACKS|SUPPORTS|UNRELATED]- (b:Opinion) RETURN DISTINCT ID(n) as id, n.argumenttext as argument, COLLECT(Type(r)) as type, b.argumenttext as reply ORDER BY id')
         .then(function(result){
-                result.records.forEach(function(record){
-                Download_all_Array.push({
-                    topic: record._fields[0]
-                });
 
-            });
+	    //if the first argument has a reply, push to reply array
+	    if (result.records[0]._fields[3] != null) {
+		var stats = getMode(result.records[0]);
+		replyArray.push({
+		    reply: result.records[0]._fields[3],
+		    relation: stats[0],
+		    agreement: stats[1]
+		});
+	    }
+	    
+	    for (j = 1; j < result.records.length; j++){
 
-	var json = JSON.stringify(Download_all_Array);//prep the data, this is essential
-	var filename = 'all_data.json';
-	var mimetype = 'application/json';
-	res.setHeader('Content-Type', mimetype);
-	res.setHeader('Content-disposition','attachment; filename='+filename);
-	res.send( json );//send the file to the client
+		//get stats (mode and agreement)
+		stats = getMode(result.records[j]);
+		//if the argument has the same id as the previous
+		if (result.records[j]._fields[0].low == result.records[j - 1]._fields[0].low) {
+		    //if it has replies add to the reply array for that argument
+		    if (result.records[j]._fields[3] != null) {
+			replyArray.push({
+			    reply: result.records[j]._fields[3],
+			    relation: stats[0],
+			    agreement: stats[1]
+			});
+		    }
+		} else {
+		    //push to data array
+		    DownloadAllArray.push({
+			argument: result.records[j-1]._fields[1],
+			replies: replyArray
+                    });
+		    //reset reply array and then add the reply for the current argument
+		    replyArray = [];
+		    if (result.records[j]._fields[3] != null) {
+			replyArray.push({
+			    reply: result.records[j]._fields[3],
+			    relation: stats[0],
+			    agreement: stats[1]
+			});
+		    }
+		}
+
+            }
+	    var obj = {"records": DownloadAllArray};
+	    var json = JSON.stringify(obj, null, 4);//prep the data, this is essential
+	    var filename = 'all_data.json';
+	    var mimetype = 'application/json';
+	    res.setHeader('Content-Type', mimetype);
+	    res.setHeader('Content-disposition','attachment; filename='+filename);
+	    res.send( json );//send the file to the client
         })
 
         .catch(function(err){
@@ -284,6 +322,60 @@ var Download_all_Array=[];
 
 });
 //end of download all ----------------------------------------------
+
+
+function getMode(record) {
+    //console.log(record);
+    var length = record._fields[2].length,
+	unrelatedCount = 0,
+	supportsCount = 0,
+	attacksCount = 0;
+    for (i = 0; i < length; i++) {
+	if (record._fields[2][i] == "UNRELATED") {
+	    unrelatedCount++;
+	} else if (record._fields[2][i] == "ATTACKS") {
+	    attacksCount++;
+	} else {
+	    supportsCount++;
+	}
+	
+    }
+    var sum = unrelatedCount + attacksCount + supportsCount;
+    //console.log(attacksCount + " " + supportsCount + " " + unrelatedCount);
+    
+    if (unrelatedCount >= supportsCount && unrelatedCount >= attacksCount) {
+	return ["UNRELATED",unrelatedCount/sum];
+	
+    } else if (supportsCount >= attacksCount && supportsCount >= unrelatedCount) {
+	return ["SUPPORTS",supportsCount/sum];
+	
+    } else {
+	return ["ATTACKS",attacksCount/sum];
+    }
+    
+    
+}
+
+//Doesn't actually seem to work - talk 2 da krew!!
+function fleissKappa(records) {
+    var n = sum(records),
+	k = records.length, // number of categories an opinion can be categorised as
+	prop = 0,
+	agreeExtent = 0;
+    //console.log(n + " " + k);
+    for (i = 0; i < k; i++) {
+	var tempProp = parseInt(records[i]._fields[1]);
+	prop += (tempProp * tempProp)/(n*n);
+	agreeExtent += (tempProp) * (tempProp);
+    }
+    //console.log(prop);
+    agreeExtent = (agreeExtent - n)/(n*(n-1));
+    //console.log(agreeExtent);
+    var kappa = (agreeExtent - prop)/(1-prop);
+
+    return kappa;
+}
+
 
 
 
